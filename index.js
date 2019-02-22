90cc31d8bb7cb346a17fa31c414e376149e0a1cf
Santiago <sgobotta@gmail.com>
2019-02-22 17:38:10 -0300

    Updates:
    + Adds babel conf for ES6 support
    + Adds faker and moment dependencies
    + Implements a transit model
    + Implements random coords functions

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const createFile = require('./create-file')
+import { createFile } from './lib/create-file'
+import { getLocation } from './lib/helpers'
 
-console.log(process.argv)
 const { argv } = process
 const modelArg = argv[2]
 const amountArg = argv[3]
+const fileName = argv[4]
 
-// TODO: Evaluate available resources and catch error
+// TODO: Evaluate modelArg is not undefined and exists in the available
+// resources.
 
+// TODO: Evaluate amount exists and is not 0
+
+// Gets the model
 const model = require(`./resources/${modelArg}`)
+
+let data
+
+// Generate a single instance or a list
+if (amountArg <= 1) {
+  const _model = model.createSingle()
+  data = _model
+}
+else {
+  const _models = model.createMany({ amount: amountArg })
+  data = _models
+}
+
+// Generate Json
+createFile(fileName, data)
--- resources/transit.js
+++ resources/transit.js
@@ -1,37 +1,91 @@
-const { numbers } = require('../lib/helpers')
-console.log('Numbers', numbers)
+import faker from 'faker'
+import moment from 'moment'
+import { getLocation, numbers } from '../lib/helpers'
 
-const createSingle = (args = {}) => {
+faker.locale = 'es'
+const { randomize } = faker.helpers
+
+const vehicleBrands = require('./../fixtures/vehicle-brands.json')
+const vehicleModels = require('./../fixtures/vehicle-models.json')
+
+const createSingle = ({ args } = {}) => {
+  const vehicleName = randomize(vehicleBrands)
+  const vehicleBrand = randomize(vehicleModels[vehicleName])
+  const dateFormat = 'DD/MM/YYYY'
+  const createdAt = faker.date.past()
+  const formmatedCreatedAt = moment(createdAt).format(dateFormat)
+  const updatedAt = faker.date.recent()
+  const formmatedUpdatedAt = moment(updatedAt).format(dateFormat)
+  const originPackage = {
+    coordinates: getLocation(),
+    address: {
+      city: faker.address.city(),
+      state: faker.address.state(),
+      streetAddress: faker.address.streetAddress(),
+      country: faker.address.country()
+    },
+    status: faker.helpers.randomize(['waiting', 'picked-up'])
+  }
+  const destinationPackage = {
+    coordinates: getLocation(),
+    address: {
+      city: faker.address.city(),
+      state: faker.address.state(),
+      streetAddress: faker.address.streetAddress(),
+      country: faker.address.country()
+    },
+    status: originPackage.status === 'waiting' ? 'waiting' : faker.helpers.randomize(['waiting', 'delivered'])
+  }
+
   const _args = {
     id: numbers.randomBetween(1, 250000),
-    transportName: "",
-    driverName: "",
+    transportName: faker.company.companyName(),
+    driverName: `${faker.name.firstName()} ${faker.name.lastName()}`,
     vehicle: {
-      name: "",
-      model: ""
+      name: vehicleName,
+      model: vehicleBrand
     },
-    createdAt: "",
-    updatedAt: "",
+    createdAt: formmatedCreatedAt,
+    updatedAt: formmatedUpdatedAt,
     originPackage: {
       coordinates: {
-        lat: "",
-        lng: ""
+        lat: originPackage.coordinates.latitude,
+        lng: originPackage.coordinates.longitude
       },
-      address: "",
-      status: ""
+      address: {
+        city: originPackage.address.city,
+        state: originPackage.address.state,
+        streetAddress: originPackage.address.streetAddress,
+        country: originPackage.address.country
+      },
+      status: originPackage.status
     },
     destinationPackage: {
       coordinates: {
-        lat: "",
-        lng: ""
+        lat: destinationPackage.coordinates.latitude,
+        lng: destinationPackage.coordinates.longitude
       },
-      address: "",
-      status: ""
+      address: {
+        city: destinationPackage.address.city,
+        state: destinationPackage.address.state,
+        streetAddress: destinationPackage.address.streetAddress,
+        country: destinationPackage.address.country
+      },
+      status: destinationPackage.status
     }
   }
   return Object.assign({}, _args, args)
 }
 
+const createMany = ({ amount, args = {} }) => {
+  const models = Array.from({ length: amount }).map((x, i) => {
+    return createSingle({ args })
+  });
+  return models
+}
+
+// We use module.exports to dynamically require them in index.js
 module.exports = {
-  createSingle
+  createSingle,
+  createMany
 }
