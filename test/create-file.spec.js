c3f18d4aa62d82eb7f487d63441bceb33a31a6df
Santiago <sgobotta@gmail.com>
2019-04-14 04:51:33 -0300

    create-file.spec: Adds a little bit refactor on the file name

--- test/create-file.spec.js
+++ test/create-file.spec.js
@@ -3,21 +3,23 @@ import path from 'path'
 import { expect } from 'chai'
 import { createFile } from '../lib/create-file'
 import exampleData from '../models/example.json'
+
 describe('Create File', () => {
+  const fileName = 'example-test.json'
+
   before('given a name \'example-test\' and a data object creates a json file', () => {
-    const fileName = 'example-test.json'
     createFile(fileName, exampleData)
   })
 
   it('createFile - a file with name \'example-test.json\' must exist', () => {
-    const filePath = path.resolve(__dirname, '../../output/example-test.json')
+    const filePath = path.resolve(__dirname, `../../output/${fileName}`)
     fs.exists(filePath, (exists) => {
       expect(exists).to.eql(true)
     })
   })
 
   after(() => {
-    const filePath = path.resolve(__dirname, '../../output/example-test.json')
+    const filePath = path.resolve(__dirname, `../../output/${fileName}`)
     fs.unlink(filePath, (err) => {
       expect(err).to.eql(null)
     })
