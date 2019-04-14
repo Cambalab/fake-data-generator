84675c4d06212a3ccc2f359fc2dea448d8a5ebd8
Santiago <sgobotta@gmail.com>
2019-04-14 11:33:55 -0300

    createFile test: polishes syntax and variables usage

--- test/create-file.spec.js
+++ test/create-file.spec.js
@@ -6,22 +6,23 @@ import exampleData from '../models/example.json'
 
 describe('Create File', () => {
   const fileName = 'example-test.json'
+  const outputDir = path.resolve(__dirname, `../../output/${fileName}`)
 
-  before('given a name \'example-test\' and a data object creates a json file', () => {
+  before('given a name \'example-test\' and a data object creates a json file with a model\'s data', () => {
     createFile(fileName, exampleData)
   })
 
-  it('createFile - a file with name \'example-test.json\' must exist', () => {
-    const filePath = path.resolve(__dirname, `../../output/${fileName}`)
+  it('createFile - a json file with name \'example-test.json\' must exist', () => {
+    const filePath = outputDir
     fs.exists(filePath, (exists) => {
       expect(exists).to.eql(true)
     })
   })
 
   after(() => {
-    const filePath = path.resolve(__dirname, `../../output/${fileName}`)
-    fs.unlink(filePath, (err) => {
-      expect(err).to.eql(null)
+    const filePath = outputDir
+    fs.unlink(filePath, (error) => {
+      expect(error).to.eql(null)
     })
   })
 })
