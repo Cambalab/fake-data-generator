ebc3c7a936a6ccd8d2eb089ef12ec7a529aab75c
sgobotta <santiago@camba.coop>
2020-07-02 12:13:45 -0300

    :computer: few changes to use faker date between

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import { description, homepage, name, version } from './package.json'
-import generateModel from './lib'
+import generateModel, { validate } from './lib'
 
 module.exports = {
   description,
   name,
   version,
   doc: `Visit ${homepage} for more information.`,
+  validateModel: validate,
   generateModel
 }
